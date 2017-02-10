8af0b6260212f95bbfaab97bf4f5b377d0e040bd
allytng <fnz.tastic@gmail.com>
2017-02-10 11:33:32 +0800

    add plot1.R

---
+++ plot1.R
@@ -0,0 +1,34 @@
+# Reconstruct plot 1 to examine how household energy usage varies over a 2-day period in February, 2007 (2007-02-01 and 2007-02-02).
+
+## Download data into working directory, if not already done 
+
+dataset_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if(!file.exists("power.zip")) {
+        download.file(dataset_url, "power.zip")
+}
+
+if(!file.exists("household_power_consumption.txt")) {
+        unzip("power.zip")
+}
+
+## Read data into R
+
+fulldata <- read.table("household_power_consumption.txt", na.strings = "?", sep = ";", header = TRUE)
+
+## Convert the Date and Time variables to Date/Time classes 
+
+fulldata$Time <- strptime(paste(fulldata$Date, fulldata$Time), "%d/%m/%Y %H:%M:%S")
+fulldata$Date <- as.Date(fulldata$Date, "%d/%m/%Y")
+
+## Subset data to required dates in Feb: 2007-02-01 and 2007-02-02
+
+data <- fulldata[fulldata$Date == "2007-02-01" | fulldata$Date == "2007-02-02", ]
+
+## Construct plot 1 (histogram) and save in png file with width of 480 pixels and height of 480 pixels. 
+
+png(filename = "plot1.png", width = 480, height = 480)
+
+hist(data$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
+
+dev.off()
